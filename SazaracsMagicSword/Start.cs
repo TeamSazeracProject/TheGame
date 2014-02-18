0015274ca9480dfe5e0d5b30a51790c8a964813a
Siko91 <lokikko@mail.bg>
2014-02-19 00:46:20 +0200

    Update Start.cs

--- SazaracsMagicSword/Start.cs
+++ SazaracsMagicSword/Start.cs
@@ -15,7 +15,6 @@ namespace SazaracsMagicSword
             //1) Main menu (new game / load game)
             //2) Dynamic game
             //3) In-game menu
-            
             //4) Conversations
             //5) Battles
             //6) Levels
