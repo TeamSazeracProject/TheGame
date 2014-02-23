ffae0dbe6c29b583c370e93cc1c382539a0ea80b
https://github.com/Siko91 <lokikko@mail.bg>
2014-02-23 21:15:27 +0200

    cosmetic changes

---
+++ SazaracsMagicSword/RunTime/MenuGameOver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SazaracsMagicSword.RunTime
+{
+    class MenuGameOver
+    {
+        // will be done later
+
+
+        /*
+            // Starts a new instance of the program itself
+            System.Diagnostics.Process.Start(Application.ExecutablePath);
+
+            // Closes the current process
+            Environment.Exit(0);
+         */
+
+        /*
+
+         _______  _______  _______  _______    _______           _______  _______
+        (  ____ \(  ___  )(       )(  ____ \  (  ___  )|\     /|(  ____ \(  ____ )
+        | (    \/| (   ) || () () || (    \/  | (   ) || )   ( || (    \/| (    )|
+        | |      | (___) || || || || (__      | |   | || |   | || (__    | (____)|
+        | | ____ |  ___  || |(_)| ||  __)     | |   | |( (   ) )|  __)   |     __)
+        | | \_  )| (   ) || |   | || (        | |   | | \ \_/ / | (      | (\ (   
+        | (___) || )   ( || )   ( || (____/\  | (___) |  \   /  | (____/\| ) \ \__
+        (_______)|/     \||/     \|(_______/  (_______)   \_/   (_______/|/   \__/
+
+
+         */
+    }
+}
