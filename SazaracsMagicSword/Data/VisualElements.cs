29c62558bb062474f1bd81c316d70bf13f635b26
D3BEPOT <dejanmk@mail.bg>
2014-02-26 09:43:33 +0200

    class VisualElements
    Set random field to static

--- SazaracsMagicSword/Data/VisualElements.cs
+++ SazaracsMagicSword/Data/VisualElements.cs
@@ -6,49 +6,55 @@ namespace SazaracsMagicSword.Data
 {
     class VisualElements
     {
-        Random rnd = new Random();
+        static Random rnd = new Random();
+
+        #region Elements constructors
         public VisualElement Empty(int size)
         {
             return MakeElement(ConsoleColor.Black, size, size, ' ', ' ', true);
         }
+
         public VisualElement Hero(int size)
         {
             return MakeElement(ConsoleColor.White, size, size, '*', '*', true);
         }
+
         public VisualElement Barren(int size)
         {
             return MakeElement(ConsoleColor.DarkGray, size, size, ' ', '.', false);
         }
+
         public VisualElement Desert(int size)
         {
             return MakeElement(ConsoleColor.Yellow, size, size, ' ', '.', false);
         }
+
         public VisualElement Grass(int size)
         {
             return MakeElement(ConsoleColor.Green, size, size, ' ', '.', false);
         }
+
         public VisualElement Rock(int size)
         {
             return MakeElement(ConsoleColor.DarkGray, size, size, '#', '#', true);
         }
+
         public VisualElement NPC(int size)
         {
             return MakeElement(ConsoleColor.DarkRed, size, size, '?', '!', true);
         }
+        #endregion
 
-        public VisualElement MakeElement(ConsoleColor color, int width, int height, char baseTexture, char randomTexture, bool isSolid)
+        #region Element building methods
+        private VisualElement MakeElement(ConsoleColor color, int width, int height, char baseTexture, char randomTexture, bool isSolid)
         {
             VisualBrick[,] visuals = GetVisualsFilledWithColor(color, width, height, baseTexture);
-
             visuals = GenerateRandomTexture(visuals, randomTexture);
-        return new VisualElement(
-            visuals,
-            null,
-            isSolid
-            );
+
+            return new VisualElement(visuals, null, isSolid);
         }
 
-        public VisualBrick[,] GetVisualsFilledWithColor(ConsoleColor color, int rows, int cols, char ch)
+        private VisualBrick[,] GetVisualsFilledWithColor(ConsoleColor color, int rows, int cols, char ch)
         {
             VisualBrick[,] bricks = new VisualBrick[rows, cols];
 
@@ -63,12 +69,12 @@ namespace SazaracsMagicSword.Data
             return bricks;
         }
 
-        public VisualBrick[,] GenerateRandomTexture(VisualBrick[,] bricks, char ch)
+        private VisualBrick[,] GenerateRandomTexture(VisualBrick[,] bricks, char ch)
         {
             int row = rnd.Next(0, bricks.GetLength(0));
             int col = rnd.Next(0, bricks.GetLength(1));
 
-            if (rnd.Next(0,4)!=0) // 20% chance for not generating random texture
+            if (rnd.Next(0, 4) != 0) // 20% chance for not generating random texture
             {
                 return bricks;
             }
@@ -77,5 +83,6 @@ namespace SazaracsMagicSword.Data
 
             return bricks;
         }
+        #endregion
     }
 }
